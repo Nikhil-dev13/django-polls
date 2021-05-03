a43c88af2ca1570ca6ae9c677244e642a46a334b
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-05-03 21:27:53 +0530

    change admin views

--- server/polls/admin.py
+++ server/polls/admin.py
@@ -3,5 +3,18 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['publish_date']}),
+    ]
+    inlines=[ChoiceInline]
+    list_display = ('question_text', 'publish_date')
+    list_filter = ['publish_date']
+
+admin.site.register(Question, QuestionAdmin)
+
